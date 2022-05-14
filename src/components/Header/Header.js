ca998c3b82902dc1c42fb2ec226b922ceaffaeb4
kyegordon3886 <kye.gordon.productions@gmail.com>
2022-05-14 18:49:21 -0500

    Added all basic sections to pages
    Links are in the nav bar
    Routing is functional
    Header has my name
    Needs email form
    Needs information in sections

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,30 +3,35 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import { Link, NavLink } from 'react-router-dom'
 
-const authenticatedOptions = (
-  <Fragment>
-    <NavLink to='/change-password' className='nav-link'>Change Password</NavLink>
-    <NavLink to='/sign-out' className='nav-link'>Sign Out</NavLink>
-  </Fragment>
-)
+// const authenticatedOptions = (
+//   <Fragment>
+//     <NavLink to='/change-password' className='nav-link'>Change Password</NavLink>
+//     <NavLink to='/sign-out' className='nav-link'>Sign Out</NavLink>
+//   </Fragment>
+// )
 
-const unauthenticatedOptions = (
-  <Fragment>
-    <NavLink to='/sign-up' className='nav-link'>Sign Up</NavLink>
-    <NavLink to='/sign-in' className='nav-link'>Sign In</NavLink>
-  </Fragment>
-)
+// const unauthenticatedOptions = (
+//   <Fragment>
+//     <NavLink to='/sign-up' className='nav-link'>Sign Up</NavLink>
+//     <NavLink to='/sign-in' className='nav-link'>Sign In</NavLink>
+//   </Fragment>
+// )
 
 const alwaysOptions = (
   <Fragment>
     <NavLink exact to='/' className='nav-link'>Home</NavLink>
+    <NavLink exact to='/about' className='nav-link'>About Me</NavLink>
+    <NavLink exact to='/skills' className='nav-link'>My Skills</NavLink>
+    <NavLink exact to='/contact' className='nav-link'>Contact Me</NavLink>
+    <NavLink exact to='/resume' className='nav-link'>My Resume</NavLink>
+    <NavLink exact to='/my-works' className='nav-link'>Projects</NavLink>
   </Fragment>
 )
 
 const Header = ({ user }) => (
   <Navbar bg='primary' variant='dark' expand='md'>
     <Navbar.Brand>
-      <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}>react-auth-template</Link>
+      <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}>Kyle Gordon Hough</Link>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls='basic-navbar-nav' />
     <Navbar.Collapse id='basic-navbar-nav'>
@@ -35,7 +40,7 @@ const Header = ({ user }) => (
           <span className='navbar-text mr-2'>Welcome, {user.email}</span>
         )}
         {alwaysOptions}
-        {user ? authenticatedOptions : unauthenticatedOptions}
+        {/* {user ? authenticatedOptions : unauthenticatedOptions} */}
       </Nav>
     </Navbar.Collapse>
   </Navbar>
